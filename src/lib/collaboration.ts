fad76e3a78864f1f579eb886bd66fe6cad3e3772
iam shadoww <iamshadoww2@gmail.com>
2025-07-09 07:02:14 +0000

    test with multiple devices

--- src/lib/collaboration.ts
+++ src/lib/collaboration.ts
@@ -1,6 +1,5 @@
 import * as Y from 'yjs';
-import { Awareness, applyAwarenessUpdate, encodeAwarenessUpdate } from 'y-protocols/awareness';
-import { Observable } from 'lib0/observable';
+import { WebsocketProvider } from 'y-websocket';
 
 const USER_COLORS = ['#A0CFEC', '#8FBC8F', '#F4A261', '#E76F51', '#2A9D8F'];
 const USER_NAMES = [
@@ -13,73 +12,8 @@ export const randomUser = () => ({
   color: USER_COLORS[Math.floor(Math.random() * USER_COLORS.length)],
 });
 
-const bcConnected = { current: false };
-
-class BroadcastChannelProvider extends Observable<any> {
-  public awareness: Awareness;
-  private bc: BroadcastChannel;
-  private doc: Y.Doc;
-
-  // Store handlers to be able to remove them on disconnect
-  private readonly _docUpdateHandler: (update: Uint8Array, origin: any) => void;
-  private readonly _awarenessChangeHandler: (changes: { added: number[], updated: number[], removed: number[] }, origin: any) => void;
-  private readonly _bcMessageHandler: (event: MessageEvent) => void;
-
-
-  constructor(roomName: string, doc: Y.Doc) {
-    super();
-    this.doc = doc;
-    this.awareness = new Awareness(doc);
-    this.bc = new BroadcastChannel(roomName);
-
-    this._docUpdateHandler = (update: Uint8Array, origin: any) => {
-      // Don't broadcast updates that came from this provider
-      if (origin !== this) {
-        this.bc.postMessage({ type: 'update', update });
-      }
-    };
-    this.doc.on('update', this._docUpdateHandler);
-
-    this._awarenessChangeHandler = ({ added, updated, removed }, origin) => {
-       // Only broadcast local awareness changes
-      if (origin === 'local') {
-        const changedClients = added.concat(updated, removed);
-        const awarenessUpdate = encodeAwarenessUpdate(this.awareness, changedClients);
-        this.bc.postMessage({
-            type: 'awareness',
-            awareness: awarenessUpdate,
-        });
-      }
-    };
-    this.awareness.on('change', this._awarenessChangeHandler);
-
-    this._bcMessageHandler = (event) => {
-      const { type, update, awareness } = event.data;
-      if (type === 'update' && update) {
-        Y.applyUpdate(this.doc, new Uint8Array(update), this);
-      } else if (type === 'awareness' && awareness) {
-        applyAwarenessUpdate(this.awareness, new Uint8Array(awareness), this);
-      }
-    };
-    this.bc.addEventListener('message', this._bcMessageHandler);
-  }
-
-  public connect() {
-    // For this mock, connect is synchronous
-    bcConnected.current = true;
-    this.emit('status', [{ status: 'connected' }]);
-  }
-
-  public disconnect() {
-    this.doc.off('update', this._docUpdateHandler);
-    this.awareness.off('change', this._awarenessChangeHandler);
-    this.bc.removeEventListener('message', this._bcMessageHandler);
-    
-    bcConnected.current = false;
-    this.emit('status', [{ status: 'disconnected' }]);
-    this.bc.close();
-  }
-}
+// Note: Using a public demo server. For production, you should host your own y-websocket server.
+const WEBSOCKET_SERVER_URL = 'wss://demos.yjs.dev';
 
 export function createWebsocketProvider(id: string, yjsDocMap: Map<string, Y.Doc>) {
   let doc = yjsDocMap.get(id);
@@ -87,5 +21,13 @@ export function createWebsocketProvider(id: string, yjsDocMap: Map<string, Y.Doc
     doc = new Y.Doc();
     yjsDocMap.set(id, doc);
   }
-  return new BroadcastChannelProvider(id, doc);
+
+  // The 'y-websocket' package handles awareness updates automatically.
+  // The provider will have an `awareness` property that the CollaborationPlugin can use.
+  const provider = new WebsocketProvider(WEBSOCKET_SERVER_URL, id, doc);
+
+  // The connect() and disconnect() methods are handled by the WebsocketProvider constructor
+  // and its destroy() method, which Lexical's CollaborationPlugin will call on unmount.
+
+  return provider;
 }
